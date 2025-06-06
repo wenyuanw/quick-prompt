911b7c54ddc5d3c45ce79bdf1b5891f4e3bcd2be
szy <tl071515@gmail.com>
2025-06-06 17:18:23 +0800

    bug: 修复挤压和换行导致的开关按钮异位

--- entrypoints/options/components/CategoryList.tsx
+++ entrypoints/options/components/CategoryList.tsx
@@ -133,7 +133,7 @@ const CategoryList = ({
                     onChange={(e) => onToggleEnabled(category.id, e.target.checked)}
                     className='sr-only peer'
                   />
-                  <div className='w-9 h-5 bg-gray-200 dark:bg-gray-600 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-600 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[""] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 dark:after:border-gray-600 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-600'></div>
+                  <div className='relative w-9 h-5 bg-gray-200 dark:bg-gray-600 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-600 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[""] after:absolute after:top-1/2 after:right-1/2 after:-translate-y-1/2 after:bg-white after:border-gray-300 dark:after:border-gray-600 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-600'></div>
                   <span className='ml-2 text-xs text-gray-600 dark:text-gray-300'>
                     {category.enabled ? '已启用' : '已停用'}
                   </span>
